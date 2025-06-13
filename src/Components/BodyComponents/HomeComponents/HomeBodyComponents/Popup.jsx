0cb8ab368dedd56b4bc462677e0b181b243b2fcb
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-06-13 20:15:57 +0530

    Minor Changes

--- src/Components/BodyComponents/HomeComponents/HomeBodyComponents/Popup.jsx
+++ src/Components/BodyComponents/HomeComponents/HomeBodyComponents/Popup.jsx
@@ -6,36 +6,42 @@ export const Popup = () => {
   if (!showPopup) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20 px-4">
+    <div
+      className="
+        flex z-200
+        px-4
+        bg-black bg-opacity-50
+        fixed inset-0 items-center justify-center
+      "
+    >
       <div
         className="
-          relative
-          bg-[#252836] text-white
           w-full max-w-sm
-          sm:max-w-md
-          mx-auto
+          mx-auto px-4 py-6
+          text-white
+          bg-[#252836]
           rounded-2xl
           shadow-lg
-          px-4 py-6
-          sm:px-6 sm:py-10
+          relative
+          sm:max-w-md sm:px-6 sm:py-10
         "
       >
         <button
           onClick={closePopup}
-          className="
-            absolute
-            top-3 right-3
-            text-[#EA7C69]
-            hover:text-white
-            focus:outline-none
-          "
           aria-label="Close popup"
+          className="
+            text-[#EA7C69]
+            absolute top-3 right-3 hover:text-white focus:outline-none
+          "
         >
           <svg
-            className="w-6 h-6 sm:w-8 sm:h-8"
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            className="
+              w-6 h-6
+              sm:w-8 sm:h-8
+            "
           >
             <path
               d="M18 6L6 18M6 6L18 18"
@@ -47,7 +53,12 @@ export const Popup = () => {
           </svg>
         </button>
 
-        <p className="text-center text-sm sm:text-base">
+        <p
+          className="
+            text-center text-sm
+            sm:text-base
+          "
+        >
           {popupMessage}
         </p>
       </div>
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: true, // listen on all interfaces (required for ngrok)
-    allowedHosts: [
-      "f8a8-2409-40d6-118f-afa6-840a-130a-16bf-b37c.ngrok-free.app",
-    ],
-  },
+  plugins: [react(), tailwindcss()]
 });
